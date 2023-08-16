50717f3c662c5be10edd02b713591da64d324ed1
vahan-sahakyan <vahan.sahakyan1996@gmail.com>
2023-08-16 20:05:40 +0400

    added an installation script

---
+++ README.md
@@ -0,0 +1,14 @@
+## Code Inspect 📦 Docker Composed
+
+### Get Started
+
+#### 1. Prerequisites
+
+- docker CLI
+- docker-compose
+
+#### 2. Installation
+
+```bash
+$ chmod +x install.sh && ./install.sh
+```
---
+++ install.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+git clone https://github.com/vahan-sahakyan/code-inspect-frontend.git frontend
+git clone https://github.com/vahan-sahakyan/code-inspect-backend.git backend
+
+docker-compose up
